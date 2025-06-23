d230894aa4c741708081049e8b6523207b47cdf8
phntmzn <126989297+phntmzn@users.noreply.github.com>
2025-06-23 12:28:54 -0500

    Add files via upload

--- source/BIT_STREAM_LIB.h
+++ source/BIT_STREAM_LIB.h
@@ -1,21 +1,16 @@
 //
 //  BIT_STREAM_LIB.h
-//  BIT-STREAM-CLI
+//  BIT-STREAM-LIB
 //
 //  Created by bRx86 on 4/13/25.
 //
-
-#ifndef BIT_STREAM_LIB_h
-#define BIT_STREAM_LIB_h
 
 #import <Foundation/Foundation.h>
 
 @interface BIT_STREAM_LIB : NSObject
 
-- (void)initializeMIDI;
+- (BOOL)initializeMIDI;
 - (void)sendBitStream:(NSData *)data;
 - (void)teardownMIDI;
 
 @end
-
-#endif /* BIT_STREAM_LIB_h */
--- source/BIT_STREAM_LIB.m
+++ source/BIT_STREAM_LIB.m
@@ -1,8 +1,51 @@
 //
 //  BIT_STREAM_LIB.m
-//  BIT-STREAM-CLI
+//  BIT-STREAM-LIB
 //
 //  Created by bRx86 on 4/13/25.
 //
 
-#import <Foundation/Foundation.h>
+#import "BIT_STREAM_LIB.h"
+#import <CoreMIDI/CoreMIDI.h>
+
+@implementation BIT_STREAM_LIB
+{
+    MIDIClientRef _midiClient;
+    MIDIPortRef _outputPort;
+    MIDIEndpointRef _destination;
+}
+
+- (BOOL)initializeMIDI {
+    OSStatus status;
+
+    status = MIDIClientCreate(CFSTR("BitStreamClient"), NULL, NULL, &_midiClient);
+    if (status != noErr) return NO;
+
+    status = MIDIOutputPortCreate(_midiClient, CFSTR("BitStreamOutput"), &_outputPort);
+    if (status != noErr) return NO;
+
+    ItemCount destCount = MIDIGetNumberOfDestinations();
+    if (destCount < 1) return NO;
+
+    _destination = MIDIGetDestination(0);
+    return YES;
+}
+
+- (void)sendBitStream:(NSData *)data {
+    if (!_destination || !_outputPort) return;
+
+    Byte buffer[1024];
+    MIDIPacketList *packetList = (MIDIPacketList *)buffer;
+    MIDIPacket *packet = MIDIPacketListInit(packetList);
+    packet = MIDIPacketListAdd(packetList, sizeof(buffer), packet, 0, data.length, data.bytes);
+
+    if (packet)
+        MIDISend(_outputPort, _destination, packetList);
+}
+
+- (void)teardownMIDI {
+    if (_outputPort) MIDIPortDispose(_outputPort);
+    if (_midiClient) MIDIClientDispose(_midiClient);
+}
+
+@end
